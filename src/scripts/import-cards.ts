ef2ef96b618bc6f8fbf3a2b6d954a1477f6ad5ca
Benjamin Roy <b.roy@groupe-connexia.com>
2026-08-19 16:20:23 +0200

    added ts-node dependence

--- src/scripts/import-cards.ts
+++ src/scripts/import-cards.ts
@@ -1,8 +1,6 @@
-import { PrismaClient } from "@prisma/client";
+import prisma from "@/lib/prisma";
 import * as fs from "fs";
 import * as path from "path";
-
-const prisma = new PrismaClient();
 
 function getCardUniqueId(cardData: any): string {
 
