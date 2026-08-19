a8e9666fa809094445f899527413f46167064b6b
Benjamin Roy <b.roy@groupe-connexia.com>
2026-08-19 11:43:09 +0200

    fix typo

--- src/app/api/reset-games/route.ts
+++ src/app/api/reset-games/route.ts
@@ -2,7 +2,7 @@ import prisma from '@/lib/prisma';
 import { NextResponse } from 'next/server';
 
 export async function GET() {
-    try {z
+    try {
         const statsDeleted = await prisma.userQueueStat.deleteMany({});
 
         const gamesDeleted = await prisma.game.deleteMany({});
